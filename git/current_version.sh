908533a8e26cef27cfdaf09f8822f1e3c707a5c4
Dennis Lee <libiyu1992@gmail.com>
2018-09-28 21:08:05 +0800

    check if as a submodule

--- git/current_version.sh
+++ git/current_version.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+cd ../..
+
+outer_repo=$(git config --get remote.origin.url)
+
+if [ -n "$outer_repo" ]; then
+    echo "This tools set is using as a submodule, all git operations would be excuted under the parent project."
+    echo -e "Current directory is: \n" $(pwd)
+else
+    echo -e "Nothing happened... Current directory is: " && cd -
+fi;
+
+echo "Current versionName is: "
+
 versionName=$(git describe 2> /dev/null)
 
 [ -z "$versionName" ] && echo "0.0.1" || echo $versionName
